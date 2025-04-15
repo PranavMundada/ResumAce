ae68f7adcd16c62f67f5e79034a01ceeac1edc7e
Pranav Mundada <pjmundada2005@gmail.com>
2025-04-15 09:44:42 +0530

    Responsive Downloading Bug Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ import "./css/info.css";
 import "./css/resume.css";
 import "./css/left.css";
 import "./css/right.css";
-import { html2pdf } from "html2pdf.js";
+// import { html2pdf } from "html2pdf.js";
 import { useRef } from "react";
 
 function App() {
-  const ref=useRef(null);
+  const ref = useRef(null);
   const [infoType, setInfoType] = useState(0);
   const [personalDetails, setPersonalDetails] = useState(
     sample.personalDetails,
@@ -43,20 +43,73 @@ function App() {
             <InfoTypeBtn onclick={showCustomize} btnName="Customize" />
           </div>
           <div id="download">
-          <button onClick={()=>{
-          const element=ref.current;
-          console.log(element);
-          const options = {
-            margin: 0,
-            filename: `${personalDetails.fullName} resume.pdf`,
-            image: { type: 'jpeg', quality: 0.98 },
-            html2canvas: { scale: 2 },
-            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
-          };
-          import("html2pdf.js").then((html2pdf) => {
-            html2pdf.default().from(element).set(options).save();
-        });
-        }}>Download Resume</button>
+            <button
+              onClick={() => {
+                const element = ref.current;
+
+                // Save current style
+                const originalStyle = element.getAttribute("style");
+
+                // Apply fixed layout styles for PDF export
+                // element.style.width = "750px"; // A4 width in px @ 96dpi
+                // element.style.height="1050px";
+                // element.style.padding = "40px";
+                // element.style.boxSizing = "border-box";
+                const screenWidth = window.innerWidth;
+
+                // Set PDF dimensions dynamically
+                let pdfFormat;
+                if (screenWidth < 400) {
+                  // Mobile
+                  pdfFormat = [3.4, 4.8]; // Half-letter or custom narrow
+                }else if (screenWidth < 650) {
+                  // Tablet
+                  pdfFormat =[4.3, 5.9]; // Slightly narrower
+                } else if (screenWidth < 900) {
+                  // Tablet
+                  pdfFormat =[6.9, 9.2]; // Slightly narrower
+                }else if (screenWidth < 1050) {
+                  // Tablet
+                  pdfFormat =[6.9, 9.2]; // Slightly narrower
+                }else if (screenWidth < 1200) {
+                  // Tablet
+                  pdfFormat =[6.9, 9.2]; // Slightly narrower
+                }else if (screenWidth < 1350) {
+                  // Tablet
+                  pdfFormat =[6.9, 9.2]; // Slightly narrower
+                }
+                 else {
+                  // Desktop
+                  pdfFormat = "a4"; // Standard A4 size
+                }
+
+                const options = {
+                  margin: 0,
+                  filename: `${personalDetails.fullName} resume.pdf`,
+                  image: { type: "jpeg", quality: 0.98 },
+                  html2canvas: { scale: 2 },
+                  jsPDF: {
+                    unit: "in",
+                    format: pdfFormat,
+                    orientation: "portrait",
+                  },
+                };
+
+                import("html2pdf.js").then((html2pdf) => {
+                  html2pdf
+                    .default()
+                    .from(element)
+                    .set(options)
+                    .save()
+                    .then(() => {
+                      // Restore original styles
+                      element.setAttribute("style", originalStyle || "");
+                    });
+                });
+              }}
+            >
+              Download Resume
+            </button>
           </div>
           <EnterInfo
             infotype={infoType}
--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+
 
 function Resume({personalDetails,educationDetails,experienceDetails,accentColor,setAccentColor,textColor,setTextColor,layout,setLayout,font,setFont,ref}){
     return(
@@ -8,15 +8,15 @@ function Resume({personalDetails,educationDetails,experienceDetails,accentColor,
                     <h1>{personalDetails.fullName}</h1>
                     <div id="otherDetails" className={layout}>
                         <div>
-                        <svg xmlns="http://www.w3.org/2000/svg"  width="20px" height="20px" fill={textColor} viewBox="0 0 128 96"  id="email"><g id="Layer_2"><path id="Icons" d="M0 11.283V8a8 8 0 0 1 8-8h112a8 8 0 0 1 8 8v3.283l-64 40zm66.12 48.11a4.004 4.004 0 0 1-4.24 0L0 20.717V88a8 8 0 0 0 8 8h112a8 8 0 0 0 8-8V20.717z"></path></g></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg"  width="15px" height="30px" fill={textColor} viewBox="0 0 128 96"  id="email"><g id="Layer_2"><path id="Icons" d="M0 11.283V8a8 8 0 0 1 8-8h112a8 8 0 0 1 8 8v3.283l-64 40zm66.12 48.11a4.004 4.004 0 0 1-4.24 0L0 20.717V88a8 8 0 0 0 8 8h112a8 8 0 0 0 8-8V20.717z"></path></g></svg>
                         <p>{personalDetails.email}</p>
                         </div>
                         <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill={textColor} viewBox="0 0 32 32" id="phone"><path d="M13.216 8.064c-.382-4.394-4.966-6.55-5.16-6.638a.974.974 0 0 0-.582-.078c-5.292.878-6.088 3.958-6.12 4.086a.99.99 0 0 0 .02.54c6.312 19.584 19.43 23.214 23.742 24.408.332.092.606.166.814.234a.99.99 0 0 0 .722-.042c.132-.06 3.248-1.528 4.01-6.316a.997.997 0 0 0-.096-.612c-.068-.132-1.698-3.234-6.218-4.33a.977.977 0 0 0-.884.21c-1.426 1.218-3.396 2.516-4.246 2.65-5.698-2.786-8.88-8.132-9-9.146-.07-.57 1.236-2.572 2.738-4.2a.998.998 0 0 0 .26-.766z"></path></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="15px" height="30px" fill={textColor} viewBox="0 0 32 32" id="phone"><path d="M13.216 8.064c-.382-4.394-4.966-6.55-5.16-6.638a.974.974 0 0 0-.582-.078c-5.292.878-6.088 3.958-6.12 4.086a.99.99 0 0 0 .02.54c6.312 19.584 19.43 23.214 23.742 24.408.332.092.606.166.814.234a.99.99 0 0 0 .722-.042c.132-.06 3.248-1.528 4.01-6.316a.997.997 0 0 0-.096-.612c-.068-.132-1.698-3.234-6.218-4.33a.977.977 0 0 0-.884.21c-1.426 1.218-3.396 2.516-4.246 2.65-5.698-2.786-8.88-8.132-9-9.146-.07-.57 1.236-2.572 2.738-4.2a.998.998 0 0 0 .26-.766z"></path></svg>
                         <p>{personalDetails.phoneNumber}</p>
                         </div>
                         <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill={textColor} viewBox="0 0 24 24" id="location"><path d="M12,2a8.009,8.009,0,0,0-8,8c0,3.255,2.363,5.958,4.866,8.819,0.792,0.906,1.612,1.843,2.342,2.791a1,1,0,0,0,1.584,0c0.73-.948,1.55-1.885,2.342-2.791C17.637,15.958,20,13.255,20,10A8.009,8.009,0,0,0,12,2Zm0,11a3,3,0,1,1,3-3A3,3,0,0,1,12,13Z"></path></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="15px" height="30px" fill={textColor} viewBox="0 0 24 24" id="location"><path d="M12,2a8.009,8.009,0,0,0-8,8c0,3.255,2.363,5.958,4.866,8.819,0.792,0.906,1.612,1.843,2.342,2.791a1,1,0,0,0,1.584,0c0.73-.948,1.55-1.885,2.342-2.791C17.637,15.958,20,13.255,20,10A8.009,8.009,0,0,0,12,2Zm0,11a3,3,0,1,1,3-3A3,3,0,0,1,12,13Z"></path></svg>
                         <p>{personalDetails.address}</p>
                         </div>
                     </div>
